816aeb20cbda76d6595b8259eb73e0501a0ef39f
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-15 13:28:50 +0100

    change to create-react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const Wrapper = styled.div`
-    font-size: 2rem;
-    text-align: center;
-`;
-
-const App: FunctionComponent = () => (
-    <Wrapper>Welcome to React TS template!</Wrapper>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;

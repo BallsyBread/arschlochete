325402166be3f1971fb6d0dd18f2644a51e95b2a
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-25 14:06:46 +0100

    remove context and provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App'
-import { AuthProvider } from './services/AuthContext'
 
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
 root.render(
     <React.StrictMode>
-        <AuthProvider>
             <App/>
-        </AuthProvider>
     </React.StrictMode>
 )

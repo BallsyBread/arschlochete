97bfa94b1a107f4dc4bacd5be9f4b050b5fa32a1
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-25 13:55:36 +0100

    add AuthProvider to App

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App'
+import { AuthProvider } from './services/AuthContext'
 
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
-root.render(<App/>)
+root.render(
+    <React.StrictMode>
+        <AuthProvider>
+            <App/>
+        </AuthProvider>
+    </React.StrictMode>
+)

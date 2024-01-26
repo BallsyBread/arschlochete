079d0119016bd626d7e16ec3564fb99c560a8c74
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-26 12:47:22 +0100

    moved getAuth() to AppState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { getAuth } from 'firebase/auth';
 import SignIn from './SignIn';
 import Home from './Home';
 
 const App: FC = () => {
+
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(getAuth().currentUser ? true : false);
+
+	getAuth().onAuthStateChanged(() => setIsAuthenticated(!isAuthenticated));
+
 	return (
 		<>
-			{getAuth().currentUser ? <Home /> : <SignIn /> }
+			{isAuthenticated ? <Home /> : <SignIn /> }
 		</>
 	);
 };

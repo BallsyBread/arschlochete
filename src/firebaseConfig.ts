7371b609f2f13f12ffb31c7e54c2bf4476728051
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-26 11:59:32 +0100

    fix firebase setup

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,3 @@
-
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 
@@ -14,10 +9,5 @@ const firebaseConfig = {
     messagingSenderId: "134439708415",
     appId: "1:134439708415:web:91bb7f506f32d53af4ceca"
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-export default auth;
+export default firebaseConfig;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { initializeApp } from "firebase/app";
 import './index.css'
 import App from './components/App'
+import firebaseConfig from './firebaseConfig';
+import { getAuth } from 'firebase/auth';
+
+const app = initializeApp(firebaseConfig);
+console.log(getAuth().currentUser)
 
 const container = document.getElementById('app-root')!
 const root = createRoot(container)

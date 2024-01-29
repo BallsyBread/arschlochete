8eb610118af6a0570db5518597e80eec517148fb
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-01-29 17:55:21 +0100

    add login functionality

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,3 +1,5 @@
+import { getApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import React, { FC, useRef } from "react";
 
 const SignIn: FC = () => {
@@ -5,12 +7,17 @@ const SignIn: FC = () => {
 	const emailField = useRef<HTMLInputElement>(null);
 	const passwordField = useRef<HTMLInputElement>(null);
 
+	const handleLogIn = (event : React.MouseEvent<HTMLButtonElement> ) => {
+		event.preventDefault();
+		signInWithEmailAndPassword(getAuth(), emailField.current!.value, passwordField.current!.value);
+	}
+
 	return (
 		<>
 			<input placeholder="e-mail" type="email" ref={emailField}/>
             <input placeholder="password" type="password" ref={passwordField}/>
-			<button>Log In</button>
-			<button>Sign In</button>
+			<button onClick={handleLogIn}>Log In</button>
+			<button>Sign Up</button>
 		</>
 	);
 };
